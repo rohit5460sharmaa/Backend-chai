de45151c0f01bdfc8158894ea9fe66dda00c0af0
Rohit Sharma <soniarohit9034@gmail.com>
2024-09-03 23:40:36 +0530

    Utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (fun) => () =>{}
+// const asyncHandler = (fun) => async() =>{}
+
+// const asyncHandler = (fun) => async(req,res,next) =>{
+//     try{
+//         await fun(req,res,next)
+//     }
+//     catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
